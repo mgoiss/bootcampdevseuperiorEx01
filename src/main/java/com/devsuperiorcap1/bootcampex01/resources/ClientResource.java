3e467084fac27da22a03f46bbed073ed418194f3
Matheus Gois <mrochagois@gmail.com>
2020-10-03 23:59:52 -0300

    CRUD findall e findById

--- src/main/java/com/devsuperiorcap1/bootcampex01/resources/ClientResource.java
+++ src/main/java/com/devsuperiorcap1/bootcampex01/resources/ClientResource.java
@@ -1,15 +1,45 @@
 package com.devsuperiorcap1.bootcampex01.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperiorcap1.bootcampex01.dto.ClientDTO;
 import com.devsuperiorcap1.bootcampex01.services.ClientService;
 
 @RestController
-@RequestMapping(value = "clients")
+@RequestMapping(value = "/clients")
 public class ClientResource {
 	
 	@Autowired
 	private ClientService client;
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,	
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy			
+			) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = client.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO dto = client.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- src/main/java/com/devsuperiorcap1/bootcampex01/services/ClientService.java
+++ src/main/java/com/devsuperiorcap1/bootcampex01/services/ClientService.java
@@ -1,8 +1,15 @@
 package com.devsuperiorcap1.bootcampex01.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperiorcap1.bootcampex01.dto.ClientDTO;
+import com.devsuperiorcap1.bootcampex01.entities.Client;
 import com.devsuperiorcap1.bootcampex01.repositories.ClientRepository;
 
 @Service
@@ -10,5 +17,18 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(client -> new ClientDTO(client));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
+	}
 	
 }
